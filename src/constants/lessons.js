54e8482ef461f179bbf010cc3aff4fa09346f425
Suren <khe4oyan@gmail.com>
2025-09-16 09:55:25 +0400

    .

--- src/constants/lessons.js
+++ src/constants/lessons.js
@@ -24,12 +24,17 @@ class DoubleLess {
 
     // calculate weeknumber from September 1
     const startTime = new Date(2025, 8, 1).getTime();
-    const nowTime = new Date().getTime();
+    const nowDate = new Date();
+    const nowTime = nowDate.getTime();
     const passedTime = nowTime - startTime;
 
     const passedDays = Math.floor(passedTime / 1000 / 60 / 60 / 24);
-    const passedWeeks = Math.floor(passedDays / 7);
-    
+    let passedWeeks = Math.floor(passedDays / 7);
+
+    if (nowDate.getDay() > 4) {
+      ++passedWeeks;
+    }
+
     if (passedWeeks % 2 === 0) {
       return less_odd;
     } else {
