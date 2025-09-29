0d1c58a0e770043d5bcf78200b0948bad547bee9
Suren <khe4oyan@gmail.com>
2025-09-29 19:05:11 +0400

    [style] fix styles

--- src/components/DoubleLessonChanger/DoubleLessonChanger.jsx
+++ src/components/DoubleLessonChanger/DoubleLessonChanger.jsx
@@ -7,7 +7,7 @@ export default function DoubleLessonChanger({ setIsTopLesson, isTopLesson }) {
   return (
     <button className={classes.root} onClick={onClick}>
       <p className={`${!isTopLesson && classes.inActiveP}`}>Համարիչ</p>
-      <div className={`${classes.border} ${isTopLesson && classes.borderActive}`}></div>
+      <div className={`${classes.border} ${!isTopLesson && classes.borderActive}`}></div>
       <p className={`${isTopLesson && classes.inActiveP}`}>Հայտարար</p>
     </button>
   )
