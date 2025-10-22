48ed3a7a6adbeefcfc0b8102adbf3e51e01069fe
Suren <khe4oyan@gmail.com>
2025-10-22 14:43:49 +0400

    RELEASE VERSION

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className={classes.root}>
       <h1 className={classes.headerText}>Դասացուցակ</h1>
-      <p className={classes.beta}>beta</p>
+      {/* <p className={classes.beta}>beta</p> */}
     </header>
   )
 }
