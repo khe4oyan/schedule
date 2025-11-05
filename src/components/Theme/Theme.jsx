be648b3c01f77223fc2efb288004c54712ec1db3
Suren <khe4oyan@gmail.com>
2025-11-05 19:44:06 +0400

    [refac]

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -4,18 +4,14 @@ import useVersion from '../../customHooks/useVersion';
 // components
 import NotificationCircle from '../NotificationCircle';
 
+// constants
+import themesData from '../../constants/themes.js';
+
 // styles
 import classes from './styles.module.css';
 
 export default function Theme({ newTheme }) {
-  const buttons = [
-    { name: "Gold & Black", theme: "colors_gold_black", version: 1, customIcon: true },
-    { name: "Glass", theme: "colors_glass", version: 2, customIcon: true },
-    { name: "Space", theme: "colors_space", version: 1 },
-    { name: "Night", theme: "colors_dark", version: 1 },
-    { name: "Pink", theme: "colors_pink", version: 1 },
-    { name: "Light", theme: "colors_default", version: 0 },
-  ];
+  const buttons = themesData;
 
   const onClickTheme = (className) => {
     newTheme(className);
---
+++ src/constants/themes.js
@@ -0,0 +1,10 @@
+const themesData = [
+  { name: "Gold & Black", theme: "colors_gold_black", version: 1, customIcon: true },
+  { name: "Glass", theme: "colors_glass", version: 2, customIcon: true },
+  { name: "Space", theme: "colors_space", version: 1 },
+  { name: "Night", theme: "colors_dark", version: 1 },
+  { name: "Pink", theme: "colors_pink", version: 1 },
+  { name: "Light", theme: "colors_default", version: 0 },
+];
+
+export default themesData;
