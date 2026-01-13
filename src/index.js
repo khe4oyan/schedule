c5113246592cf27f8b6382cb2990eb092d07953a
khe4oyan <khe4oyan@gmail.com>
2026-01-13 16:02:50 +0400

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import AllComponentsPreview from './components/AllComponentsPreview';
+import AllComponentsPreview from './components/AllComponentsPreview';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-// root.render(<AllComponentsPreview />);
+root.render(<AllComponentsPreview />);
