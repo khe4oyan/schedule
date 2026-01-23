26bac62db090c3e5ec240d62510d497147e442f7
khe4oyan <khe4oyan@gmail.com>
2026-01-23 18:02:46 +0400

    .

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -12,12 +12,34 @@ export default function Settings() {
         optionKey="headerShow"
         defaultValue={true}
         isRequiredToReload={true}
-      />
+        />
 
       <SettingsOption 
         optionName="Անջատել դասի գրադիենտը"
         optionKey="lessonLineDisabled"
       />
+
+      <div className={classes.lastUpdateDateContainer}>
+        <p className={classes.lastUpdateDate}>Թարմացվել է</p>
+        <p className={classes.lastUpdateDate}> {timerFormat("2026-01-23 17:00")} առաջ</p>
+      </div>
     </div>
   )
+}
+
+function timerFormat(inputDate) {
+  const updateDate = new Date(inputDate);
+  const currentDate = new Date();
+
+  const diffSeconds = Math.floor((currentDate - updateDate) / 1000);
+  if (diffSeconds < 60) return `${diffSeconds} վայրկյան`;
+
+  const diffMinutes = Math.floor(diffSeconds / 60);
+  if (diffMinutes < 60) return `${diffMinutes} րոպե`;
+
+  const diffHours = Math.floor(diffMinutes / 60);
+  if (diffHours < 24) return `${diffHours} ժամ`;
+
+  const diffDays = Math.floor(diffHours / 24);
+  return `${diffDays} օր`;
 }
