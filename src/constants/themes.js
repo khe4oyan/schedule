19c7e47156bcf19817146fcfdb41bb38686ec8b7
khe4oyan <khe4oyan@gmail.com>
2026-02-15 10:46:36 +0400

    [chore] cut some themes

--- src/constants/themes.js
+++ src/constants/themes.js
@@ -1,11 +1,8 @@
 const themesData = [
   { name: "Cyber Slate", theme: "colors_cyber_slate", version: 1, customIcon: true },
-  { name: "Martix", theme: "colors_matrix", version: 1, customIcon: true },
   { name: "Gold & Black", theme: "colors_gold_black", version: 1, customIcon: true },
   { name: "Glass", theme: "colors_glass", version: 2, customIcon: true },
   { name: "Space", theme: "colors_space", version: 1 },
-  { name: "Night", theme: "colors_dark", version: 1 },
-  { name: "Pink", theme: "colors_pink", version: 1 },
   { name: "Light", theme: "colors_default", version: 0 },
 ];
 
