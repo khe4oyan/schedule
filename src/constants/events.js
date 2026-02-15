556bb9c136f8ec6c3885032aac9d8c763c397a31
khe4oyan <khe4oyan@gmail.com>
2026-02-15 13:18:16 +0400

    [fix] date type format change to ISO for IOS

--- src/constants/events.js
+++ src/constants/events.js
@@ -2,8 +2,8 @@
 import Event from '../classes/Event.class.js';
 
 const events = [
-  new Event("Դատարան", "Նազարբեկյան 40", "2026.02.20 10:00"),
-  new Event("Դատարան", "Նազարբեկյան 40", "2026.02.27 10:00"),
+  new Event("Դատարան", "Նազարբեկյան 40", "2026-02-20 10:00"),
+  new Event("Դատարան", "Նազարբեկյան 40", "2026-02-27 10:00"),
 ];
 
 export default events;
