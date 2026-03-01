9521c7aa53cccc6692742f48d538f69ec4c445b3
khe4oyan <khe4oyan@gmail.com>
2026-03-01 15:39:54 +0400

    [chore] fix

--- src/constants/events.js
+++ src/constants/events.js
@@ -4,7 +4,7 @@ import Event from "../classes/Event.class.js";
 const allEvents = [
   new Event("Դատարան", "Նազարբեկյան 40", "2026-02-20 10:00"),
   new Event("Դատարան", "Նազարբեկյան 40", "2026-02-27 10:00"),
-  new Event("Դատարան", "Նազարբեկյան 40", "2026-10-10 10:00"),
+  new Event("Դատարան", "Նազարբեկյան 40", "2026-03-10 10:00"),
 ];
 
 const prevEvents = [];
