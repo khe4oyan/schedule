30cf11554b3317fde0e573d0534f49a311261dd0
khe4oyan <khe4oyan@gmail.com>
2026-04-11 14:02:46 +0400

    [chore] cut version

--- src/components/Storage/Storage.jsx
+++ src/components/Storage/Storage.jsx
@@ -1,22 +1,12 @@
 // libs
+import { useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
-
-// components
-import NotificationCircle from "../NotificationCircle";
-
-// hooks
-import useVersion from "../../customHooks/useVersion";
-
-// constants
-import VERSIONS from "../../constants/versions";
 
 // styles
 import classes from "./styles.module.css";
-import { useEffect, useState } from "react";
 
 export default function Storage() {
   const [storageLength, setStorageLength] = useState(localStorage.length);
-  const [isShowUpdate, updateVersion] = useVersion("storage", VERSIONS.STORAGE);
 
   const onClearStorage = () => {
     if (storageLength === 0) return alert("Ջնջելու տվյալ չկա"); 
@@ -27,17 +17,8 @@ export default function Storage() {
     }
   };
 
-  useEffect(() => {
-    if (isShowUpdate) {
-      return () => {
-        updateVersion();
-      };
-    }
-  }, []);
-
   return (
     <div className={classes.root}>
-      {isShowUpdate && <NotificationCircle /> }
       <p>Ներկա պահին կա {storageLength} տվյալ</p>
       <p>Կարող ես ջնջել տվյալները, որ ավելորդ բան չմնա</p>
       <button onClick={onClearStorage} className={classes.deleteButton}>
--- src/constants/versions.js
+++ src/constants/versions.js
@@ -2,7 +2,6 @@ const VERSIONS = {
   EVENTS: 3,
   THEME: 7,
   SETTINGS: 4,
-  STORAGE: 1,
 };
 
 export default VERSIONS;
